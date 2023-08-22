90585015151e27e2f48d2b79359f454c678690bb
CruzIsaiah <isaiahcruz13@yahoo.com>
2023-08-22 12:27:47 -0400

    - Implemented a graphical user interface (GUI) for the Hangman Game. - Updated the word bank with new words and definitions.

--- main.py
+++ main.py
@@ -1,63 +1,139 @@
 import random
+import tkinter as tk
 from definitions import word_definitions
 
-play_again = True
 
-while play_again:
-    # Choose a random word and its definition from the dictionary
-    word, definition = random.choice(list(word_definitions.items()))
+class HangmanGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("HM Study Game")
 
-    # Display the definition as a hint at the beginning of the game
-    print("Definition:", definition)
+        self.incorrect_guesses = 0
+        self.max_incorrect_guesses = 6
+        self.guessed_letters = []
+        self.guessed_word = []
+        self.is_playing = False
 
-    # Initialize the game
-    incorrect_guesses = 0
-    max_incorrect_guesses = 6
-    guessed_letters = []
+        self.create_widgets()
 
-    # Loop until the player either wins or loses
-    while incorrect_guesses < max_incorrect_guesses:
-        # Show the word with underscores for unguessed letters and spaces as is
-        word_to_show = ""
-        for letter in word:
-            if letter.lower() in guessed_letters or letter == " ":
-                word_to_show += letter
-            else:
-                word_to_show += "_"
-        print(word_to_show)
+    def create_widgets(self):
+        self.definition_label = tk.Label(
+            self.root, text="", font=("Helvetica", 12))
+        self.definition_label.pack()
 
-        # Check if the player has won
-        if "_" not in word_to_show:
-            print("You win!")
-            break
+        self.word_label = tk.Label(self.root, text="")
+        self.word_label.pack()
 
-        # Prompt the player to make a guess
-        guess = input("Guess a letter: ").lower()
-        if len(guess) != 1 or not guess.isalpha():
-            print("Invalid input. Please enter a single character.")
-            continue
-        if guess in guessed_letters:
-            print("You have already guessed that letter. Please guess another letter.")
-            continue
-        guessed_letters.append(guess)
-        if guess in word.lower():  # Convert word to lowercase for comparison
-            # Correct guess
-            continue
+        self.guess_label = tk.Label(self.root, text="Guess a letter: ")
+        self.guess_label.pack()
+
+        self.guess_entry = tk.Entry(self.root)
+        self.guess_entry.pack()
+
+        self.guess_button = tk.Button(
+            self.root, text="Guess", command=self.check_guess)
+        self.guess_button.pack()
+
+        self.message_label = tk.Label(self.root, text="")
+        self.message_label.pack()
+
+        self.guessed_letters_label = tk.Label(
+            self.root, text="Guessed Letters:")
+        self.guessed_letters_label.pack()
+
+        self.guessed_letters_display = tk.Label(self.root, text="")
+        self.guessed_letters_display.pack()
+
+        self.play_again_button = tk.Button(
+            self.root, text="Play Again", command=self.play_again)
+        self.play_again_button.pack()
+        self.play_again_button.config(state='disabled')
+
+        self.start_game()
+
+    def start_game(self):
+        self.word, self.definition = random.choice(
+            list(word_definitions.items()))
+        self.guessed_word = ["_"] * len(self.word)
+        self.is_playing = True
+        self.incorrect_guesses = 0
+        self.guessed_letters = []
+
+        self.definition_label.config(text=f"Hint: {self.definition}")
+        self.update_word_to_show()
+        self.guess_button.config(state='active')
+        self.play_again_button.config(state='disabled')
+
+    def check_guess(self):
+        if not self.is_playing:
+            return
+
+        guess = self.guess_entry.get().lower()
+
+        if not guess:
+            self.guess_entry.delete(0, 'end')
+            return
+
+        if len(guess) != 1 or (not guess.isalpha() and guess != " " and guess != "+"):
+            self.message_label.config(
+                text="Invalid input. Please enter a single letter, a space, or a '+' character.")
+            self.guess_entry.delete(0, 'end')
+            return
+
+        if guess in self.guessed_letters:
+            self.message_label.config(
+                text="You have already guessed that letter. Please guess another letter.")
+            self.guess_entry.delete(0, 'end')
+            return
+
+        self.guessed_letters.append(guess)
+        self.update_guessed_letters_display()
+
+        if guess in self.word.lower():
+            self.update_word_to_show(guess)
         else:
-            # Incorrect guess
-            incorrect_guesses += 1
-            # Guess warning
-        if incorrect_guesses == 5:
-            print("Careful one guess left!!")
+            self.incorrect_guesses += 1
 
-        # Check if the player has lost
-        if incorrect_guesses == max_incorrect_guesses:
-            print("You lose! The word was", word)
-            break
+        if self.incorrect_guesses == 5:
+            self.message_label.config(text="Careful one guess left!!")
 
-    # Ask the user if they want to play again
-    play_again_input = input("Do you want to play again (y/n)? ")
-    if play_again_input.lower() != "y":
-        play_again = False
+        if self.incorrect_guesses == self.max_incorrect_guesses:
+            self.message_label.config(
+                text=f"You lose! The word was {self.word}")
+            self.end_game()
+        elif "_" not in self.word_label.cget("text"):
+            self.message_label.config(text="You win!")
+            self.end_game()
 
-print("Goodbye!")
+        self.guess_entry.delete(0, 'end')
+
+    def update_word_to_show(self, guess=None):
+        new_word_to_show = ""
+        for i in range(len(self.word)):
+            if guess is not None and (self.word[i].lower() == guess or self.word[i] == " "):
+                self.guessed_word[i] = self.word[i]
+            new_word_to_show += self.guessed_word[i] + " "
+
+        self.word_label.config(text=new_word_to_show)
+
+    def update_guessed_letters_display(self):
+        self.guessed_letters_display.config(
+            text=", ".join(self.guessed_letters))
+
+    def end_game(self):
+        self.guess_button.config(state='disabled')
+        self.play_again_button.config(state='active')
+        self.is_playing = False
+
+    def play_again(self):
+        self.definition_label.config(text="")
+        self.message_label.config(text="")
+        self.guessed_letters_display.config(text="")
+        self.play_again_button.config(state='disabled')
+        self.start_game()  # Call start_game to get a new hint
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = HangmanGame(root)
+    root.mainloop()
